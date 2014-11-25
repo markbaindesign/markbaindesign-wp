9c383b148d46e6df864dae92386c20324036c1b0
markbaindesign <hello@markbaindesign.com>
2014-11-25 10:18:41 +0100

    tweaks to menu

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,7 @@ module.exports = function(grunt) {
         // watch for changes and trigger sass, jshint, uglify and livereload
         watch: {
             sass: {
+					options: { sourcemap: true },
                 files: ['assets/sass/**/*.{scss,sass}'],
                 tasks: ['sass', 'autoprefixer']
             },
