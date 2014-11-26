1f484360e3ad8633568038b60b3dc1f44b792721
markbaindesign <hello@markbaindesign.com>
2014-11-26 09:48:12 +0100

    use wordpress function in link to favis

--- httpdocs/wp-content/themes/markbaindesign/header.php
+++ httpdocs/wp-content/themes/markbaindesign/header.php
@@ -13,21 +13,21 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
-<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
-<link rel="icon" type="image/png" href="/favicon-196x196.png" sizes="196x196">
-<link rel="icon" type="image/png" href="/favicon-160x160.png" sizes="160x160">
-<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
-<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
-<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+<link rel="apple-touch-icon" sizes="57x57" href="<?php echo esc_url( home_url( '/' ) ); ?>/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="114x114" href="<?php echo esc_url( home_url( '/' ) ); ?>/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="72x72" href="<?php echo esc_url( home_url( '/' ) ); ?>/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="144x144" href="<?php echo esc_url( home_url( '/' ) ); ?>/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="60x60" href="<?php echo esc_url( home_url( '/' ) ); ?>/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="120x120" href="<?php echo esc_url( home_url( '/' ) ); ?>/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="76x76" href="<?php echo esc_url( home_url( '/' ) ); ?>/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="152x152" href="<?php echo esc_url( home_url( '/' ) ); ?>/apple-touch-icon-152x152.png">
+<link rel="icon" type="image/png" href="<?php echo esc_url( home_url( '/' ) ); ?>/favicon-196x196.png" sizes="196x196">
+<link rel="icon" type="image/png" href="<?php echo esc_url( home_url( '/' ) ); ?>/favicon-160x160.png" sizes="160x160">
+<link rel="icon" type="image/png" href="<?php echo esc_url( home_url( '/' ) ); ?>/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="<?php echo esc_url( home_url( '/' ) ); ?>/favicon-16x16.png" sizes="16x16">
+<link rel="icon" type="image/png" href="<?php echo esc_url( home_url( '/' ) ); ?>/favicon-32x32.png" sizes="32x32">
 <meta name="msapplication-TileColor" content="#da532c">
-<meta name="msapplication-TileImage" content="/mstile-144x144.png">
+<meta name="msapplication-TileImage" content="<?php echo esc_url( home_url( '/' ) ); ?>/mstile-144x144.png">
 	<?php wp_head(); ?>
 </head>
 
