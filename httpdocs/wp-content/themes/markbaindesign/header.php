0ac14a23dfb5b41342a6ce33c1f76b8d76d6c7d8
markbaindesign <hello@markbaindesign.com>
2014-11-26 15:42:11 +0100

    fix broken icon links in header, add open graph protocols

--- httpdocs/wp-content/themes/markbaindesign/header.php
+++ httpdocs/wp-content/themes/markbaindesign/header.php
@@ -13,21 +13,30 @@
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
-<link rel="apple-touch-icon" sizes="57x57" href="<?php echo esc_url( home_url( '/' ) ); ?>/apple-touch-icon-57x57.png">
-<link rel="apple-touch-icon" sizes="114x114" href="<?php echo esc_url( home_url( '/' ) ); ?>/apple-touch-icon-114x114.png">
-<link rel="apple-touch-icon" sizes="72x72" href="<?php echo esc_url( home_url( '/' ) ); ?>/apple-touch-icon-72x72.png">
-<link rel="apple-touch-icon" sizes="144x144" href="<?php echo esc_url( home_url( '/' ) ); ?>/apple-touch-icon-144x144.png">
-<link rel="apple-touch-icon" sizes="60x60" href="<?php echo esc_url( home_url( '/' ) ); ?>/apple-touch-icon-60x60.png">
-<link rel="apple-touch-icon" sizes="120x120" href="<?php echo esc_url( home_url( '/' ) ); ?>/apple-touch-icon-120x120.png">
-<link rel="apple-touch-icon" sizes="76x76" href="<?php echo esc_url( home_url( '/' ) ); ?>/apple-touch-icon-76x76.png">
-<link rel="apple-touch-icon" sizes="152x152" href="<?php echo esc_url( home_url( '/' ) ); ?>/apple-touch-icon-152x152.png">
-<link rel="icon" type="image/png" href="<?php echo esc_url( home_url( '/' ) ); ?>/favicon-196x196.png" sizes="196x196">
-<link rel="icon" type="image/png" href="<?php echo esc_url( home_url( '/' ) ); ?>/favicon-160x160.png" sizes="160x160">
-<link rel="icon" type="image/png" href="<?php echo esc_url( home_url( '/' ) ); ?>/favicon-96x96.png" sizes="96x96">
-<link rel="icon" type="image/png" href="<?php echo esc_url( home_url( '/' ) ); ?>/favicon-16x16.png" sizes="16x16">
-<link rel="icon" type="image/png" href="<?php echo esc_url( home_url( '/' ) ); ?>/favicon-32x32.png" sizes="32x32">
+
+<!-- Favicons & icons -->
+<link rel="apple-touch-icon" sizes="57x57" href="<?php echo esc_url( home_url() ); ?>/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="114x114" href="<?php echo esc_url( home_url() ); ?>/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="72x72" href="<?php echo esc_url( home_url() ); ?>/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="144x144" href="<?php echo esc_url( home_url() ); ?>/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="60x60" href="<?php echo esc_url( home_url() ); ?>/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="120x120" href="<?php echo esc_url( home_url() ); ?>/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="76x76" href="<?php echo esc_url( home_url() ); ?>/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="152x152" href="<?php echo esc_url( home_url() ); ?>/apple-touch-icon-152x152.png">
+<link rel="icon" type="image/png" href="<?php echo esc_url( home_url() ); ?>/favicon-196x196.png" sizes="196x196">
+<link rel="icon" type="image/png" href="<?php echo esc_url( home_url() ); ?>/favicon-160x160.png" sizes="160x160">
+<link rel="icon" type="image/png" href="<?php echo esc_url( home_url() ); ?>/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="<?php echo esc_url( home_url() ); ?>/favicon-16x16.png" sizes="16x16">
+<link rel="icon" type="image/png" href="<?php echo esc_url( home_url() ); ?>/favicon-32x32.png" sizes="32x32">
 <meta name="msapplication-TileColor" content="#da532c">
-<meta name="msapplication-TileImage" content="<?php echo esc_url( home_url( '/' ) ); ?>/mstile-144x144.png">
+<meta name="msapplication-TileImage" content="<?php echo esc_url( home_url() ); ?>/mstile-144x144.png">
+
+<!-- Open Graph protocol -->
+<meta property="og:image" content="<?php echo get_stylesheet_directory_uri(); ?>/img/mbd-og.png">
+<meta property="og:image:width" content="1200">
+<meta property="og:image:height" content="630">
+<meta property="og:image:type" content="image/png">
+
 	<?php wp_head(); ?>
 </head>
 
