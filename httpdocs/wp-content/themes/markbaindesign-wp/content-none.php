d43b507143ec9e8595abc1519afed5d2ca516723
markbain <hello@markbaindesign.com>
2015-01-26 11:53:48 +0100

    add 404 template

--- httpdocs/wp-content/themes/markbaindesign-wp/content-none.php
+++ httpdocs/wp-content/themes/markbaindesign-wp/content-none.php
@@ -8,13 +8,13 @@
  */
 ?>
 
-<div class="section no-results not-found">
-	<div class="container">
-		<header class="page-header">
-			<h1 class="page-title"><?php _e( 'Nothing Found', '_mbbasetheme' ); ?></h1>
-		</header><!-- .page-header -->
+<article class="no-results not-found">
 
-	<div class="page-content">
+		<header class="entry-header">
+			<h1 class="entry-title"><?php _e( 'Nothing Found', '_mbbasetheme' ); ?></h1>
+		</header><!-- .entry-header -->
+
+	<div class="entry-content">
 		<?php if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
 
 			<p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', '_mbbasetheme' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
@@ -31,6 +31,5 @@
 			<?php get_search_form(); ?>
 
 		<?php endif; ?>
-	</div><!-- .page-content -->
-</div><!-- .container -->
-</div><!-- .section -->
+	</div><!-- .entry-content -->
+</div><!-- .no-results .not-found -->
