976558efe7a637385d1dbaa56ca5ffae46fc5bc1
markbain <markbaindesign@gmail.com>
2024-02-21 12:20:12 +0100

    Fix favicons

--- public_html/wp-content/themes/bain-design-theme/header.php
+++ public_html/wp-content/themes/bain-design-theme/header.php
@@ -15,21 +15,13 @@
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 
 	<!-- Favicons & icons -->
-	<link rel="apple-touch-icon" sizes="57x57" href="<?php echo esc_url( home_url() ); ?>/apple-touch-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="<?php echo esc_url( home_url() ); ?>/apple-touch-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="<?php echo esc_url( home_url() ); ?>/apple-touch-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="<?php echo esc_url( home_url() ); ?>/apple-touch-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="<?php echo esc_url( home_url() ); ?>/apple-touch-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="<?php echo esc_url( home_url() ); ?>/apple-touch-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="<?php echo esc_url( home_url() ); ?>/apple-touch-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="<?php echo esc_url( home_url() ); ?>/apple-touch-icon-152x152.png">
-	<link rel="icon" type="image/png" href="<?php echo esc_url( home_url() ); ?>/favicon-196x196.png" sizes="196x196">
-	<link rel="icon" type="image/png" href="<?php echo esc_url( home_url() ); ?>/favicon-160x160.png" sizes="160x160">
-	<link rel="icon" type="image/png" href="<?php echo esc_url( home_url() ); ?>/favicon-96x96.png" sizes="96x96">
-	<link rel="icon" type="image/png" href="<?php echo esc_url( home_url() ); ?>/favicon-16x16.png" sizes="16x16">
-	<link rel="icon" type="image/png" href="<?php echo esc_url( home_url() ); ?>/favicon-32x32.png" sizes="32x32">
-	<meta name="msapplication-TileColor" content="#da532c">
-	<meta name="msapplication-TileImage" content="<?php echo esc_url( home_url() ); ?>/mstile-144x144.png">
+   <link rel="icon" type="image/png" sizes="32x32" href="<?php echo esc_url( home_url() ); ?>/favicon-32x32.png?v=2.5.0">
+   <link rel="icon" type="image/png" sizes="16x16" href="<?php echo esc_url( home_url() ); ?>/favicon-16x16.png?v=2.5.0">
+   <link rel="manifest" href="<?php echo esc_url( home_url() ); ?>/site.webmanifest?v=2.5.0">
+   <link rel="mask-icon" href="<?php echo esc_url( home_url() ); ?>/safari-pinned-tab.svg?v=2.5.0" color="#000000">
+   <link rel="shortcut icon" href="<?php echo esc_url( home_url() ); ?>/favicon.ico?v=2.5.0">
+   <meta name="msapplication-TileColor" content="#000000">
+   <meta name="theme-color" content="#ffffff">
 
 	<!-- Open Graph protocol -->
 	<meta property="og:image" content="<?php echo get_stylesheet_directory_uri(); ?>/img/mbd-og.png">
